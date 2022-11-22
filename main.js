1a5f5d7401f602db8958e0ae881034994d1c7af5
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-11-21 21:14:25 -0300

    Updates

---
+++ main.js
@@ -0,0 +1,37 @@
+import home from './pages/home/home.js';
+import register from './pages/register/register.js';
+import password from './pages/password/password.js';
+import about from './pages/about/about.js';
+import feed from './pages/feed/feed.js';
+
+const main = document.querySelector('#root');
+
+const inicio = () => {
+  window.addEventListener('hashchange', () => {
+    main.innerHTML = '';
+    switch (window.location.hash) {
+      case '':
+        main.appendChild(home());
+        break;
+      case '#recuperarsenha':
+        main.appendChild(password());
+        break;
+      case '#paracadastro':
+        main.appendChild(register());
+        break;
+      case '#sobre':
+        main.appendChild(about());
+        break;
+      case '#feed':
+        main.appendChild(feed());
+        break;
+      default:
+        main.appendChild(home());
+    }
+  });
+};
+window.addEventListener('load', () => {
+  window.location.hash = '';
+  main.appendChild(home());
+  inicio();
+});
